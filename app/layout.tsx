2ebaaafdc1853a82286edc957be077a325140115
Leonardo <leonardomancmia5511@gmail.com>
2026-07-29 22:48:20 -0300

    gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,15 +34,22 @@ export default async function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-
-        {/* Google tag (gtag.js) */}
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-J9M01N1C4G"></script>
-  <script dangerouslySetInnerHTML={{ __html: `
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-J9M01N1C4G');
-  `}} />
+     {/* Se o GTM estiver cadastrado no painel, injeta o script do Google */}
+        {gtmId && (
+          <Script
+            id="google-tag-manager"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${gtmId}');
+              `,
+            }}
+          />
+        )}
 
         {/* Se o GTM estiver cadastrado no painel, injeta o script do Google */}
         {gtmId && (
